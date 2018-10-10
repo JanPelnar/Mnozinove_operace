ca62986267f148b3214300e2e198464eac2cc89a
JanPelnar <43808777+JanPelnar@users.noreply.github.com>
2018-10-10 12:20:08 +0200

    Update Sjednoceni_mnozin.java

--- Sjednoceni_mnozin.java
+++ Sjednoceni_mnozin.java
@@ -1,1 +1,29 @@
+public class Sjednoceni_mnozin {
+    int pocetPrvku1 = 1;
+    int pocetPrvku2 = 2;
+    int[] mnozina1 = new int[pocetPrvku1];
+    int[] mnozina2 = new int[pocetPrvku2];    
+    
+    
+    
+    int odpocet = 0;
+    int i = 0;
+    public void Sjednoceni_mnozin(){
+        mnozina1[0] = 5;
+        mnozina2[0] = 9;
+        mnozina2[1] = 1;
+        int[] sjednoceni = new int[pocetPrvku1 + pocetPrvku2];
+            while(odpocet < pocetPrvku1){
+                sjednoceni[odpocet] = mnozina1[odpocet];
+                odpocet++;
+            }
+            while(odpocet < (pocetPrvku1 + pocetPrvku2)){
+                sjednoceni[odpocet] = mnozina2[i];
+                odpocet++;
+                i++;
+                System.out.println(odpocet);
+                System.out.println(sjednoceni);
+            }
+        }
+    }        
 
