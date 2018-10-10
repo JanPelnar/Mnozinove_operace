0830ac2929af20383a54c3cc74ad4f915e29228a
JakubMazac <43809087+JakubMazac@users.noreply.github.com>
2018-10-10 11:21:28 +0200

    Update Generovani_mnozin.java

--- Generovani_mnozin.java
+++ Generovani_mnozin.java
@@ -1,1 +1,27 @@
 
+package mnoziny;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+public class Generovani_mnozin {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    
+    public void generovani(){
+        System.out.println("Zadejte počet prvků množiny, rozmezí je od 10 do 20");
+        int pocetPrvku = sc.nextInt();
+        int odpocet = 0;
+        if (pocetPrvku>=10 && pocetPrvku<=20){
+        int[] mnozina = new int[pocetPrvku];
+        while (odpocet<pocetPrvku){
+            mnozina[odpocet] = 
+            odpocet++;
+        }
+        }
+        else {
+            System.out.println("počet prvků nesplňuje požadavky");
+        }
+        
+    }
+}
