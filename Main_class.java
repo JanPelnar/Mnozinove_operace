b902d7a6550c34cf23b63451e70b8623107a0440
JanPelnar <43808777+JanPelnar@users.noreply.github.com>
2018-10-17 12:14:44 +0200

    Update Main_class.java

--- Main_class.java
+++ Main_class.java
@@ -1,1 +1,14 @@
 
+package withkaja;
+import java.util.Arrays;
+import static withkaja.Generovani_mnozin.generovaniMnoziny;
+import withkaja.Sjednoceni_mnozin;
+public class Main_class {
+
+    public static void main(String[] args) {
+        int[] mnozina1 = generovaniMnoziny();
+        int[] mnozina2 = generovaniMnoziny();
+        Sjednoceni_mnozin x = new Sjednoceni_mnozin();
+        x.Sjednoceni_mnozin(mnozina1,mnozina2);
+    }
+}
