34fa8693fc9693e37e4ba0897063a1a80c095ef5
JakubMazac <43809087+JakubMazac@users.noreply.github.com>
2018-10-17 12:02:25 +0200

    Update Generovani_mnozin.java

--- Generovani_mnozin.java
+++ Generovani_mnozin.java
@@ -2,21 +2,20 @@ package mnoziny;
 import java.util.Scanner;
 import java.util.Random;
 public class Generovani_mnozin {
-
-    Scanner sc = new Scanner(System.in);
-    Random rd = new Random();
-    public void generovaniPrvniMnoziny(){
+    static Scanner sc = new Scanner(System.in);
+    static Random rd = new Random();
+    public static int[] generovaniMnoziny(){
         System.out.println("Zadejte počet prvků množiny, rozmezí je od 10 do 20");
-        int pocetPrvku1 = 0;
+        int pocetPrvku1 = sc.nextInt();
         while (pocetPrvku1<10||pocetPrvku1>20){
                     pocetPrvku1 = sc.nextInt();
         }
         int odpocet = 0;
         int[] mnozina1 = new int[pocetPrvku1];
         while (odpocet<pocetPrvku1){
-            mnozina1[odpocet]=rd.nextInt(30)-20;
+            mnozina1[odpocet]=rd.nextInt(31)-20;
             odpocet++;
         }
-        System.out.println(mnozina1);
+        return mnozina1;
         }
     }
