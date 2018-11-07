a28a9e214ae04a096130c316d51feb62b6e1f10e
JanPelnar <43808777+JanPelnar@users.noreply.github.com>
2018-11-07 12:19:59 +0100

    Update Main_class.java

--- Main_class.java
+++ Main_class.java
@@ -1,14 +1,18 @@
-
 package withkaja;
-import java.util.Arrays;
+import static withkaja.Doplnek_mnozin.Doplnek_mnoziny1;
+import static withkaja.Doplnek_mnozin.Doplnek_mnoziny2;
+import static withkaja.Prunik_mnozin.Prunik_mnozin;
+import static withkaja.Sjednoceni_mnozin.Sjednoceni_mnozin;
+import static withkaja.Rozdil_mnozin.Rozdil_mnoziny1;
+import static withkaja.Rozdil_mnozin.Rozdil_mnoziny2;   
 import static withkaja.Generovani_mnozin.generovaniMnoziny;
-import withkaja.Sjednoceni_mnozin;
+import static withkaja.Vypis_mnozin.Vypis;
 public class Main_class {
 
     public static void main(String[] args) {
         int[] mnozina1 = generovaniMnoziny();
         int[] mnozina2 = generovaniMnoziny();
-        Sjednoceni_mnozin x = new Sjednoceni_mnozin();
-        x.Sjednoceni_mnozin(mnozina1,mnozina2);
+        Vypis(Doplnek_mnoziny1(mnozina1,mnozina2));
+
     }
 }
