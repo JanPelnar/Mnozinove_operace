bc997088030dddac7b991b896756dcc118096921
JanPelnar <43808777+JanPelnar@users.noreply.github.com>
2018-11-07 12:16:07 +0100

    Update Vypis_mnozin.java

--- Vypis_mnozin.java
+++ Vypis_mnozin.java
@@ -1,1 +1,10 @@
 
+package withkaja;
+
+import java.util.Arrays;
+
+public class Vypis_mnozin {
+    public static void Vypis(int[] mnozina){
+        System.out.println(Arrays.toString(mnozina));
+    }
+}
