59a1f5ba8b4e11adb67236c3aad5976388e0b7d9
JakubMazac <43809087+JakubMazac@users.noreply.github.com>
2018-11-14 11:15:21 +0100

    Update Vypis_mnozin.java

--- Vypis_mnozin.java
+++ Vypis_mnozin.java
@@ -1,10 +1,26 @@
-
-package withkaja;
-
+package mnoziny;
 import java.util.Arrays;
-
+import static mnoziny.Doplnek_mnozin.Doplnek_mnoziny1;
+import static mnoziny.Doplnek_mnozin.Doplnek_mnoziny2;
+import static mnoziny.Generovani_mnozin.generovaniMnoziny;
+import static mnoziny.Prunik_mnozin.Prunik_mnozin;
+import static mnoziny.Rozdil_mnozin.Rozdil_mnoziny2;
+import static mnoziny.Rozdil_mnozin.Rozdil_mnoziny1;
+import static mnoziny.Sjednoceni_mnozin.Sjednoceni_mnozin;
 public class Vypis_mnozin {
-    public static void Vypis(int[] mnozina){
-        System.out.println(Arrays.toString(mnozina));
+        int[] mnozina1 = generovaniMnoziny();
+        int[] mnozina2 = generovaniMnoziny();
+        public static void Vypis(int[] mnozina1,int[] mnozina2){
+        System.out.println(Arrays.toString(mnozina1));
+        System.out.println(Arrays.toString(mnozina2));
+        Rozdil_mnoziny1(mnozina1,mnozina2);
+        Rozdil_mnoziny2(mnozina1,mnozina2);
+        Sjednoceni_mnozin(mnozina1,mnozina2);
+        Prunik_mnozin(mnozina1,mnozina2);
+        System.out.print("Doplněk množiny A je: ");
+        Doplnek_mnoziny1(mnozina1,mnozina2);
+        System.out.print("Doplněk množiny B je: ");
+        Doplnek_mnoziny2(mnozina1,mnozina2);
     }
+        
 }
