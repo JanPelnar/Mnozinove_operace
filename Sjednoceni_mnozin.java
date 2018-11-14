63e7f45a480e1f4fce855196c91eef4bf183517b
JanPelnar <43808777+JanPelnar@users.noreply.github.com>
2018-11-14 12:05:12 +0100

    Update Sjednoceni_mnozin.java

--- Sjednoceni_mnozin.java
+++ Sjednoceni_mnozin.java
@@ -1,73 +1,80 @@
-package mnoziny;
-import java.util.Arrays;
-public class Sjednoceni_mnozin {
 
-    
+    package withkaja;
+    import java.util.Arrays;
+    public class Sjednoceni_mnozin {
 
-    public static int[] Sjednoceni_mnozin(int[] mnozina1,int[] mnozina2){
-    
-    int odpocet = 0;
-    int i = 0;
-        int[] sjednoceni = new int[mnozina1.length + mnozina2.length];
-            while(odpocet < mnozina1.length){
-                sjednoceni[odpocet] = mnozina1[odpocet];
-                odpocet++;
-            }
-            while(odpocet < (mnozina1.length + mnozina2.length)){
-                sjednoceni[odpocet] = mnozina2[i];
-                i++;
-                odpocet++;
-            }
-              int index = 0; 
-              int pozice = 0; 
-        while(pozice < (mnozina1.length + mnozina2.length)){ 
-                  i = 0;   
-            int maximum = sjednoceni[index]; 
-            while(i < (mnozina1.length + mnozina2.length)){ 
-                if(sjednoceni[i] < maximum){
-                    maximum = sjednoceni[i];
-                    sjednoceni[i] = sjednoceni[index];
-                    sjednoceni[index] = maximum;
+
+
+        public static void Sjednoceni_mnozin(int[] mnozina1,int[] mnozina2){
+
+        int odpocet = 0;
+        int i = 0;
+            int[] sjednoceni = new int[mnozina1.length + mnozina2.length];
+                while(odpocet < mnozina1.length){
+                    sjednoceni[odpocet] = mnozina1[odpocet];
+                    odpocet++;
                 }
-                i++;
-            } 
-            pozice++; 
-            index++;       
-        }
-        pozice = 0;
-        int zdvojennych = 0;
-        while(pozice < (mnozina1.length + mnozina2.length-1)){
-            if(sjednoceni[pozice] == sjednoceni[pozice+1]){
-                sjednoceni[pozice] = -31;
-                zdvojennych++;
-            }
-            pozice++;
-            
-        }
-        int[] sp = new int[mnozina1.length + mnozina2.length - zdvojennych];      
-        index = 0; 
-        pozice = 0; 
-        while(pozice < (mnozina1.length+mnozina2.length)){ 
-                  i = 0;   
-            int maximum = sjednoceni[index]; 
-            while(i < (mnozina1.length + mnozina2.length)){ 
-                if(sjednoceni[i] < maximum){ 
-                    maximum = sjednoceni[i]; 
-                    sjednoceni[i] = sjednoceni[index]; 
-                    sjednoceni[index] = maximum; 
+                while(odpocet < (mnozina1.length + mnozina2.length)){
+                    sjednoceni[odpocet] = mnozina2[i];
+                    i++;
+                    odpocet++;
+                }
+                  int index = 0; 
+                  int pozice = 0; 
+            while(pozice < (mnozina1.length + mnozina2.length)){ 
+                      i = 0;   
+                int maximum = sjednoceni[index]; 
+                while(i < (mnozina1.length + mnozina2.length)){ 
+                    if(sjednoceni[i] < maximum){ 
+                        maximum = sjednoceni[i]; 
+                        sjednoceni[i] = sjednoceni[index]; 
+                        sjednoceni[index] = maximum; 
+                    } 
+                    i++; 
                 } 
-                i++; 
-            } 
-            pozice++; 
-            index++;       
-        }
-        pozice = 0;
-        while(pozice < (mnozina1.length + mnozina2.length - zdvojennych)){
-            sp[pozice] = sjednoceni[pozice];
-            pozice++;
-        }
+                pozice++; 
+                index++;       
+            }
+            pozice = 0;
+            int zdvojennych = 0;
+            while(pozice < (mnozina1.length + mnozina2.length-1)){
+                if(sjednoceni[pozice] == sjednoceni[pozice+1]){
+                    sjednoceni[pozice] = -31;
+                    zdvojennych++;
+                }
+                pozice++;
+
+            }
+            System.out.println(zdvojennych);
+
+            int[] sp = new int[mnozina1.length + mnozina2.length - zdvojennych];      
+            index = 0; 
+            pozice = 0; 
+            while(pozice < (mnozina1.length+mnozina2.length)){ 
+                      i = 0;   
+                int maximum = sjednoceni[index]; 
+                while(i < (mnozina1.length + mnozina2.length)){ 
+                    if(sjednoceni[i] < maximum){ 
+                        maximum = sjednoceni[i]; 
+                        sjednoceni[i] = sjednoceni[index]; 
+                        sjednoceni[index] = maximum; 
+                    } 
+                    i++; 
+                } 
+                pozice++; 
+                index++;       
+            }
+            pozice = 0;
+            while(pozice < (mnozina1.length + mnozina2.length - zdvojennych)){
+                sp[pozice] = sjednoceni[pozice];
+                pozice++;
+            }
         System.out.print("Sjednocení množin je: ");
         System.out.println(Arrays.toString(sp));
-        return sp;
+
         }
-    }        
+        
+
+    
+        }        
+
